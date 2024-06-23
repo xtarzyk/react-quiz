092db444655942721e5e8b68cadeaaf0e4983c29
xtarzyk <xtarzyk@tutanota.com>
2024-06-23 18:32:21 +0200

    logo and Main comp. added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './index.css'
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   
   return (
     <div>
-      <DateCounter />
+      <Header />
+      <Main />
     </div>
   )
 }
