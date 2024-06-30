87078e4932588cb5133ad527962ee8f23d223c60
xtarzyk <xtarzyk@tutanota.com>
2024-06-30 10:49:06 +0200

    finishscreen done

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ import StartScreen from './components/StartScreen';
 import Question from './components/Question';
 import NextButton from './components/NextButton';
 import Progress from './components/Progress';
+import FinishScreen from './components/FinishScreen';
 
 const initialState = {
   questions: [],
-
-  // loading, error, ready, active, finished
   status: "loading",
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0
 }
 
 function reducer(state, action) {
@@ -48,11 +48,15 @@ function reducer(state, action) {
       }
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null }
+    case "finish":
+      return { ...state, status: 'finished', highscore: state.points > state.highscore ? state.points : state.highscore }
+    case "restart":
+      return { ...state, status: 'ready', index: 0, points: 0, answer: null}
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState)
 
   const numQuestions = questions.length
   const maxPossiblePoints = questions.reduce((acc, question) => acc += question.points, 0)
@@ -81,9 +85,17 @@ function App() {
               answer={answer}
             />
             <Question question={questions[index]} answer={answer} dispatch={dispatch} />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton dispatch={dispatch} answer={answer} numQuestions={numQuestions} index={index} />
           </>
         )}
+        {status === "finished" &&
+          <FinishScreen 
+            points={points} 
+            maxPossiblePoints={maxPossiblePoints} 
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        }
       </Main>
     </div>
   )
